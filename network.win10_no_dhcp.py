0ec4207c4845b91df476fc4ceb802127be57135a
sallecta <sallecta@inbox.ru>
2018-03-25 10:23:41 +0300

    Update network.win10_no_dhcp.py

--- network.win10_no_dhcp.py
+++ network.win10_no_dhcp.py
@@ -1,1 +1,16 @@
 
+# 1 solution
+netsh winsock reset catalog  (Reset WINSOCK entries to installation defaults)
+netsh int ipv4 reset reset.log (Reset IPv4 TCP/IP stack to installation defaults)
+
+# 2 solution
+netsh int tcp set heuristics disabled
+netsh int tcp set global autotuninglevel=disabled
+netsh int tcp set global rss=enabled
+
+# 3 solution
+check firewall/antivirus
+
+
+
+
